596286161d8f514a3ba168e1055b53d7ab277ca3
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-07 19:37:03 +0000

    fixing some indenting

--- arrayMapping.js
+++ arrayMapping.js
@@ -12,12 +12,8 @@ var result = input.map(function (currVal, index, array) {
   var z = (Math.pow(x,2) + Math.pow(y,2));
   var ans = Math.sqrt(z);
   return ans;
-
   // return currVal.x ** 2 + currVal.y ** 2;
 });
-
-
-console.log(result);
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
