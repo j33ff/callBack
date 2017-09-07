dd33de3a86f70b90f27fc4a281be17a4dc60b26f
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-07 18:25:59 +0000

    adding refactoring with forEach loop

--- callBackArg.js
+++ callBackArg.js
@@ -1,11 +1,11 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);
-        // execute callback
+  arr.forEach(function(currVal,index,array){
+    if (currVal === "Waldo") {
+      found(index);
+      // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
